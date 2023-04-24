150d1c26d5eb5c10b39f487344c634f303494da5
Omondi Agunga <agunga3d@gmail.com>
2023-04-24 11:34:27 +0300

    Encode sentence bug fix

--- src/main/java/ke/co/saf/Cipher.java
+++ src/main/java/ke/co/saf/Cipher.java
@@ -5,8 +5,8 @@ public class Cipher {
     private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public char encodeChar(char c, int key){
         int index = alphabet.indexOf(c);
-        // System.out.println("Index of " + c + " is " + index);
-        return alphabet.charAt((index + key)%26);
+        System.out.println("Index of " + c + " is " + index);
+        return alphabet.charAt((index + key + 26)%26);
     }
 
     public char decodeChar(char c, int key){
@@ -15,9 +15,15 @@ public class Cipher {
 
     public String encodeSentence(String sentence, int key){
 
-        for (int i = 0; i<alphabet.length(); i++){
-            sentence.replace(alphabet.charAt(i), encodeChar(alphabet.charAt(i), key));
+        StringBuilder builder = new StringBuilder(sentence);
+        for (int i = 0; i<sentence.length(); i++){
+            if(alphabet.indexOf(sentence.charAt(i)) != -1)
+                builder.setCharAt(i, encodeChar(sentence.charAt(i), key));
         }
-        return sentence;
+        return builder.toString();
+    }
+
+    public String decodeSentence(String encodedString, int key) {
+        return encodeSentence( encodedString, key*-1+26);
     }
 }
--- src/main/java/ke/co/saf/Main.java
+++ src/main/java/ke/co/saf/Main.java
@@ -5,7 +5,13 @@ package ke.co.saf;
 public class Main {
     public static void main(String[] args) {
         Cipher cipher = new Cipher();
-        System.out.println("Encode V => " + cipher.encodeChar('V', 2));
-        System.out.println("Decode X => " + cipher.decodeChar('X', 2));
+//        System.out.println("Encode V => " + cipher.encodeChar('V', 2));
+//        System.out.println("Decode X => " + cipher.decodeChar('X', 2));
+        String input = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        String encoded = cipher.encodeSentence(input, 23);
+        String decoded = cipher.decodeSentence(input, 23);
+
+        System.out.println("Encode: " + input + "\nOutput: " + encoded);
+        System.out.println("Decode: " + encoded + "\nOutput: " + decoded);
     }
 }
